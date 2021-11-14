9239ec68511d7a9ec88bf9066f50885629209b57
Bharatgupta922 <gbharat922@gmail.com>
2021-11-14 13:34:29 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import 'tailwindcss/tailwind.css'
+import React , {useEffect,useState} from 'react';
+import { Layout } from '../components';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <Component {...pageProps} />
+  )};
 
 export default MyApp
